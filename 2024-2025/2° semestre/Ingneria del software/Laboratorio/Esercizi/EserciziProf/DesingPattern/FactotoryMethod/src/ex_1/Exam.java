196535081fbb009de72f66db8050989153800f5d
Luigi Domenico Castano <115658266+luigicast0408@users.noreply.github.com>
2025-04-05 11:05:26 +0200

    feat(DP FactoryMethod): add first example DP Factory Method

---
+++ 2024-2025/2° semestre/Ingneria del software/Laboratorio/Esercizi/EserciziProf/DesingPattern/FactotoryMethod/src/ex_1/Exam.java
@@ -0,0 +1,15 @@
+package ex_1;
+
+public class Exam {
+    private String subject;
+    private int vote;
+
+    public Exam(String subject, int vote){
+        this.subject = subject;
+        this.vote = vote;
+    }
+
+    public  int getVote(){
+        return vote;
+    }
+}
