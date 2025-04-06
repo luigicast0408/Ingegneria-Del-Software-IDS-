155bd0c83923dd7d4d5501c1835bb38ad6ac948d
Luigi Domenico Castano <115658266+luigicast0408@users.noreply.github.com>
2025-04-06 11:06:43 +0200

    feat(DP Builder): add example of DP Builder

---
+++ 2024-2025/2° semestre/Ingneria del software/Laboratorio/Esercizi/EserciziProf/DesingPattern/Builder/src/example_1/InitTicket.java
@@ -0,0 +1,11 @@
+package example_1;
+
+public class InitTicket {
+    public Ticket makeConcreteTicket(){
+        return new Ticket.TicketBuilder()
+                .setEventName("Uscita")
+                .setDate("08-06-2025")
+                .setPrice(20.0)
+                .build();
+    }
+}
---
+++ 2024-2025/2° semestre/Ingneria del software/Laboratorio/Esercizi/EserciziProf/DesingPattern/Builder/src/example_1/Main.java
@@ -0,0 +1,9 @@
+package example_1;
+
+public class Main {
+    public static void main(String[] args) {
+        InitTicket director = new InitTicket();
+        Ticket ticket = director.makeConcreteTicket();
+        System.out.println(ticket);
+    }
+}
