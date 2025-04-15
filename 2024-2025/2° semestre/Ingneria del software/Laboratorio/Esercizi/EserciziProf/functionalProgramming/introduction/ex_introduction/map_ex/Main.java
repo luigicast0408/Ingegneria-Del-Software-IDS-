f95e809867e1f3d2589019926f86327cc2589155
Luigi Domenico Castano <115658266+luigicast0408@users.noreply.github.com>
2025-04-15 08:23:56 +0200

    fix(functionalProgramming): fix example of sum words

--- 2024-2025/2° semestre/Ingneria del software/Laboratorio/Esercizi/EserciziProf/functionalProgramming/introduction/ex_introduction/map_ex/Main.java
+++ 2024-2025/2° semestre/Ingneria del software/Laboratorio/Esercizi/EserciziProf/functionalProgramming/introduction/ex_introduction/map_ex/Main.java
@@ -1,7 +1,6 @@
 package map_ex;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,15 +9,20 @@ public class Main {
                 new Person("Ciccio", 21)
         );
 
-        List<Integer> lengths = friends.stream()
-                .map(person -> person.getName().length())
-                .toList();
-
         Person mario = new Person("Mario", 25, friends);
 
         System.out.println("Name: " + mario.getName());
         System.out.println("Age: " + mario.getAge());
         System.out.println("Sum friends age: " + mario.getSumAgesOfFriends());
         System.out.println("Sum friends age alternative: " + mario.getSumAgesOfFriendsAlternative());
+
+        List<Integer> lengths = friends.stream()
+                .map(person -> person.getName().length())
+                .toList();
+
+        int sumWords = lengths.stream().reduce(0, Integer::sum);
+        System.out.println("Sum words: " + sumWords);
+        System.out.println("Avg words: " + sumWords/lengths.size());
+        System.out.println(lengths);
     }
 }
