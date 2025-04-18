ee5368408f162921a1e4fb13c8e2b73181887a07
Luigi Domenico Castano <115658266+luigicast0408@users.noreply.github.com>
2025-04-18 11:36:45 +0200

    feat(H1.4): add ex H1.4

---
+++ 2024-2025/2° semestre/Ingneria del software/Laboratorio/Esercizi/EserciziProposti/H1.4/src/H1_4/Ex14.java
@@ -0,0 +1,15 @@
+package H1_4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Ex14 {
+    public static void main(String[] args) {
+        List<Integer> isSorted = Arrays.asList(2, 3, 5, 6, 7);
+
+        boolean isOrdered = IntStream.range(0, isSorted.size() - 1)
+                .noneMatch(i -> isSorted.get(i) > isSorted.get(i + 1));
+        System.out.println("The list is sorted: " + isOrdered);
+    }
+}
---
+++ 2024-2025/2° semestre/Ingneria del software/Laboratorio/Esercizi/EserciziProposti/H1.4/src/H1_4/text.md
@@ -0,0 +1,8 @@
+## Esercizio 5
+
+Data una lista di numeri interi positivi, verificare se la lista è ordinata.
+
+**Suggerimenti:**
+
+- Si generano gli indici da 0 a n-1.
+- Per ciascun valore dell'indice `i`, si confrontano l'elemento con indice `i` e il successivo. Se il secondo è minore del primo, la lista non è ordinata e si può fermare la verifica.
