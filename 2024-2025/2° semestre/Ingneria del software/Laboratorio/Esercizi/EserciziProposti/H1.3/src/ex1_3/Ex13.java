0114820c496de1c840dabeceaa65469a23ffa4e4
Luigi Domenico Castano <115658266+luigicast0408@users.noreply.github.com>
2025-04-18 11:36:23 +0200

    feat(H1.3): add ex H1.3

---
+++ 2024-2025/2° semestre/Ingneria del software/Laboratorio/Esercizi/EserciziProposti/H1.3/src/ex1_3/Ex13.java
@@ -0,0 +1,22 @@
+package ex1_3;
+
+import java.util.*;
+
+public class Ex13 {
+    public static void main(String[] args) {
+        List<int[]> terneList = Arrays.asList(
+                new int[]{2, 3, 4},
+                new int[]{1, 1, 3},
+                new int[]{5, 5, 5}
+        );
+
+        List<Boolean> result = terneList.stream()
+                .map(triangle -> isTriangle(triangle[0], triangle[1], triangle[2]))
+                .toList();
+        System.out.println(result);
+    }
+
+    public static boolean isTriangle(int a, int b, int c) {
+        return a + b > c && a + c > b && b + c > a;
+    }
+}
---
+++ 2024-2025/2° semestre/Ingneria del software/Laboratorio/Esercizi/EserciziProposti/H1.3/src/ex1_3/text.md
@@ -0,0 +1,15 @@
+### Esercizio 1.3
+
+Data una lista di terne di numeri interi, per ciascuna terna verificare se essa può costituire un triangolo e restituire la lista dei perimetri delle terne che rappresentano triangoli.
+
+In un triangolo, ciascun lato deve essere minore della somma degli altri due. Una terna può essere rappresentata come un array di tre elementi interi, ad esempio:
+
+```java
+int[] t = new int[] { 2, 3, 4 };
+```
+
+La lista di terne può essere rappresentata come una lista di array di interi:
+
+```java
+List<int[]> lista = new ArrayList<>();
+```
